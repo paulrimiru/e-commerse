6434e3fdd2c5f023f7e4e40badce3bbcb15657c1
paul rimiru <paulrimiru@gmail.com>
2020-05-26 07:33:18 +0300

    fix(build): fix failing build issue

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,10 +6,22 @@ import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RolesModule } from '../roles/roles.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PasswordResetModule, PassportModule, RolesModule],
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PasswordResetModule,
+    PassportModule,
+    RolesModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '5h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
